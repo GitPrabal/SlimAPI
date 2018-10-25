9e6d6b3627517c6efa2fdab157246c8894b0a62a
US IT SOLUTIONS <prabal1.gupta@gmail.com>
2018-10-25 16:34:40 +0530

    first commit

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    echo  phpinfo();
+});
+
+$app->get('/id/{id}', function(Request $request,Response $response,array $args){
+return     $id = $args['id']; 
+});
+
+$app->run();
+
+
+?>
